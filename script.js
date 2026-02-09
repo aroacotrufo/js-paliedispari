34de6014377d9b834ac69dc2f9ee7452a8098a93
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-09 14:10:35 +0100

    esercizio palindromo terminato

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 let word = prompt('Scrivi una parola')
 
 //creare una funzione per capire se la parola inserita è palindroma
+function isPalindrome(word){                                    //dichiaro funzione
+    /* const  lower = word.toLowerCase()  */                    // se mi serve trasfrormare la parola tutto in minuscolo
+    const reversed = word.split('').reverse().join('')          //.split() trasfroma la stringa in un array di singole lettere 
+                                                                //.reverse() inverte l'ordine degli elementi 
+                                                                //.join() riunisce l'array in un'unica stringa
+    return word === reversed                                    //confronta la prima stringa (word) con la versione invertita (reversed)
+}
+console.log(isPalindrome(word))                                 //stampa il risultato true o false
+
